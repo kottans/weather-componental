d8d3e394aa325d3508b2d12c7951045a5f18f79a
OleksiyRudenko <oleksiy.rudenko@gmail.com>
2019-03-17 21:01:39 +0200

    Add ComponentFactory

---
+++ src/js/framework/ComponentFactory.js
@@ -0,0 +1,6 @@
+const ComponentFactory = {
+  mappings: {},
+  register: componentClass => ComponentFactory.mappings[componentClass.name] = componentClass,
+  get: componentClassName => ComponentFactory.mappings[componentClassName],
+};
+export default ComponentFactory;
