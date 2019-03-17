c7c36063faf741261bd1520ce15a7d659639884b
OleksiyRudenko <oleksiy.rudenko@gmail.com>
2019-03-17 21:12:20 +0200

    Add ComponentFactory usage

--- src/js/framework/ComponentFactory.js
+++ src/js/framework/ComponentFactory.js
@@ -4,3 +4,9 @@ const ComponentFactory = {
   get: componentClassName => ComponentFactory.mappings[componentClassName],
 };
 export default ComponentFactory;
+
+/*
+ Every component will need:
+  1) import ComponentFactory from "../framework/ComponentFactory";
+  2) ComponentFactory.register(ComponentClass); // to register component class with factory
+ */
